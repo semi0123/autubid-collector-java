183e0249cc75ef973af9cc032f203f3040bf5ab6
hyunlimkim <gusfla09.working@gmail.com>
2017-08-14 15:04:53 +0900

    입찰모듈 연결

---
+++ src/main/java/kr/co/emforce/wonderbox/module/IProcess.java
@@ -0,0 +1,14 @@
+package kr.co.emforce.wonderbox.module;
+
+public interface IProcess {
+  public final String MODULES_DIR = ((System.getProperty("modules.dir") == null) ? "" : System.getProperty("modules.dir"));
+  public final String DOWNLOAD_URL = ((System.getProperty("download.url") == null) ? "" : System.getProperty("download.url"));
+  
+  public static final String UPLOADS_DIR = System.getProperty("uploads.dir");
+  public static final String DOWNLOADS_DIR = System.getProperty("downloads.dir");
+  
+  public static final String BETA = "beta";
+  public static final String PRODUCTION = "production";
+
+  public final String AUTO_BID_WORKER = "autobid-worker.sh";
+}
